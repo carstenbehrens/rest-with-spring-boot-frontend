777aea40dbf9cdbe275bd62aa89755411ca29379
Carsten Behrens <behrens.carsten@guj.de>
2021-02-21 13:09:30 +0100

    Add tests for Table component, add fakeApiResponse to utils

---
+++ src/components/Table/index.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import Table from "./index";
+import fakeApiResponse from "../../utils/fakeApiResponse";
+
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("Calls handleProductClick when table row gets clicked", () => {
+  const handleProductClick = jest.fn();
+  act(() => {
+    render(
+      <Table
+        products={fakeApiResponse}
+        handleProductClick={handleProductClick}
+      />,
+      container
+    );
+  });
+
+  // The first product in the table
+  const tableRow = document.querySelectorAll("tr")[1];
+
+  act(() => {
+    tableRow.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(handleProductClick).toHaveBeenCalledTimes(1);
+});
---
+++ src/utils/fakeApiResponse.js
@@ -0,0 +1,18 @@
+const fakeApiResponse = [
+  {
+    id: 1,
+    title: "Konsole",
+    name: "Playstation 5",
+    description:
+      "Die PlayStation ist eine Spielkonsole des japanischen Konzerns Sony",
+  },
+  {
+    id: 9,
+    title: "Konsole",
+    name: "Nintendo Switch",
+    description:
+      "Die Nintendo Switch ist eine Spielkonsole des japanischen Herstellers Nintendo.",
+  },
+];
+
+export default fakeApiResponse;
